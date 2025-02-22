9ba0c4cc93b6e844a9b3a44ce1f12689d879c020
akshithakuruganti <144696267+akshithakuruganti@users.noreply.github.com>
2025-02-22 15:14:50 +0530

    Create database.sql

---
+++ database.sql
@@ -0,0 +1,22 @@
+create table login(
+username varchar(50),
+password varchar(50)
+);
+
+create table register(
+username varchar(50),
+password varchar(50),
+mobile Int
+);
+
+create table disease(
+age Int,
+sympton varchar(50),
+disease varchar(50)
+insert into disease(age,sympton,disease) values
+(31,"fever,diffculty breathing,fatigue","pneumonia"),
+(29,"fever,diffculty breathing,fatigue,cough","drepression"),
+(40,"fever","diabetes"),
+(55,"couh","typhoid fever"),
+(45,"fatigue","stroke");
+);
